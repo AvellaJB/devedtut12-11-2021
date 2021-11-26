437ddec37e73e32796cbdbd72f2d7a50c703311b
Jean-Baptiste Avella <jeanbaptiste.avella@gmail.com>
2021-11-26 22:11:46 +0100

    Fin du tuto

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ let tl = gsap.timeline({
         trigger : ".front-page", 
         start : "0%", 
         end : "100%",
-        markers : true,
         scrub : true,  
         pin : true,
+        markers: true, 
     }
 })
 
-tl.fromTo(".front-page", {clipPath : 'circle(5%)'}, {clipPath : 'circle(100%)'}); 
-tl.fromTo(".music-note", {scale : 0.5}, {scale: 0, opacity : 0});
+tl.fromTo(".front-page", {clipPath : 'circle(5%)'}, {clipPath : 'circle(100%)', duration :3}); 
+tl.fromTo(".music-note", {scale : 0.5}, {scale: 0, opacity : 0, duration: 1}, "-=3");
+tl.fromTo(".title", {opacity: 0}, {opacity: 1, duration: 1});
+tl.fromTo(".sub-title", {opacity: 0}, {opacity: 1, duration: 1});
